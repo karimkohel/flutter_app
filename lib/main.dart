aa745163436eacd16ecff08d15d233d90f447631
karimkohel <kareemkohel@gmail.com>
2019-08-30 23:56:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KK app"),
+        ),
+				body: Card(child: Column(children: <Widget>[
+					Image.asset('media/kalilogo.jpg'),
+					Text("Test Text")
+				],),),
+      ),
+    );
+  }
+}
